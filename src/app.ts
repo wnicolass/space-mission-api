6a49468c4a6fb8a1fb9c3bf509b446ea76db990b
wnicolass <nicolaswgl99@gmail.com>
2023-06-22 19:04:17 +0100

    feat: added morgan to log requests

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
+import morgan from 'morgan';
 import v1Router from './routes/v1/v1.router';
 
 function createApp(): Express {
   const app: Express = express();
 
   (function middlewares(): void {
+    app.use(morgan('dev'));
     app.use(express.json());
   })();
 
