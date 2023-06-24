5bcba5be5190bcc545b10cd24f4b35ce48964855
wnicolass <nicolaswgl99@gmail.com>
2023-06-24 19:54:49 +0100

    feat: start testing auth controller

--- src/controllers/auth/auth.controller.spec.ts
+++ src/controllers/auth/auth.controller.spec.ts
@@ -1,12 +1,14 @@
 import { describe, expect, it } from 'vitest';
 import request from 'supertest';
 import app from '../../app';
+// import inMemoryAuthRepository from '../../repositories/in-memory/in-memory.auth.repository';
 
 describe('Auth Controller', () => {
   describe('Test POST /auth', () => {
+    const AUTH_URL = '/v1/auth';
     it('should respond with a 200 ok', async () => {
       const response = await request(app)
-        .post('/v1/auth/signup')
+        .post(`${AUTH_URL}/signup`)
         .expect('Content-Type', /json/)
         .expect(200);
 
