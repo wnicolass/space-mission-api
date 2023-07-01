87ded28ab82d5a9c8177d2b7ee628ef74e7cc357
wnicolass <nicolaswgl99@gmail.com>
2023-07-01 21:47:11 +0100

    feat: add function to check if passwords match

---
+++ src/services/security/validate-password.spec.ts
@@ -0,0 +1,26 @@
+import { expect, it, describe, assert } from 'vitest';
+import hashPassword from './hash-password';
+import { user } from '../../tests-data/user';
+import checkPasswords from './validate-password';
+
+describe('Password validation', () => {
+  it('should return true if password matches', async () => {
+    const dbPwd = await hashPassword(user.password);
+    const passwordMatches = await checkPasswords(
+      user.password,
+      dbPwd.hashedPassword,
+    );
+
+    expect(passwordMatches).toBe(true);
+  });
+
+  it('should return false if password does not match', async () => {
+    const dbPwd = await hashPassword(user.password);
+    const passwordMatches = await checkPasswords(
+      'somerandomtext',
+      dbPwd.hashedPassword,
+    );
+
+    expect(passwordMatches).toBe(false);
+  });
+});
