5026f66fe69df2c96cc0b56047b57e274a8e350f
wnicolass <nicolaswgl99@gmail.com>
2023-07-05 19:51:13 +0100

    tests: moved jwt tests data to 'tests-data' folder

--- src/services/security/jwt/encode.spec.ts
+++ src/services/security/jwt/encode.spec.ts
@@ -1,20 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { encodeJWT } from './encode';
+import { badPayload, payload } from '../../../tests-data/jwt';
 
 describe('Encoding JWT functionality', () => {
   it('should throw a TypeError if the payload values are not of type string', async () => {
-    const badPayload: { userId: any; email: any } = {
-      userId: 123,
-      email: 'is this an email?',
-    };
     expect(encodeJWT(badPayload)).rejects.toThrowError(TypeError);
   });
 
   it('should return an encrypted jwt of type string', async () => {
-    const payload = {
-      userId: 'fsdfsdfsd',
-      email: 'jared@gmail.com',
-    };
     const jwt = await encodeJWT(payload);
     expect(jwt).toBeTypeOf('string');
     expect(jwt.split('.').length).toBe(5);
