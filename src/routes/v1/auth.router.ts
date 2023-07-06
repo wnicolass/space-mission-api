d2bbd7218d6243138ba4ced0ff7573a00a8ea05f
wnicolass <nicolaswgl99@gmail.com>
2023-07-06 22:37:11 +0100

    feat: add /signin endpoint

--- src/routes/v1/auth.router.ts
+++ src/routes/v1/auth.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import signUpController from '../../controllers/auth/signup/signup.controller';
+import signInController from '../../controllers/auth/signin/signin.controller';
 
 export default (function authRouter(): Router {
   const router = Router();
 
   router.post('/signup', signUpController.exec);
+  router.post('/signin', signInController.exec);
 
   return router;
 })();
