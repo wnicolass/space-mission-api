88427528a8a8d0bf50fb22b34ed3aafd5a8345c7
wnicolass <nicolaswgl99@gmail.com>
2023-07-06 21:53:42 +0100

    feat: create sign in tests and its tests

---
+++ src/services/auth/signin/signin.spec.ts
@@ -0,0 +1,84 @@
+import { beforeEach, describe, expect } from 'vitest';
+import { AuthTestContext, it } from '../../../tests-data/common/test-context';
+import inMemoryUserAuthRepository from '../../../repositories/in-memory/in-memory.auth.repository';
+import { signInFactory } from './signin';
+import {
+  userWithEmptyField,
+  validUser,
+  userWithInvalidEmail,
+} from '../../../tests-data/user';
+import hashPassword from '../../security/hash/hash-password';
+
+describe('Sign In Service', () => {
+  beforeEach<AuthTestContext>(async (ctx) => {
+    const inMemoryUserAuth = inMemoryUserAuthRepository();
+    ctx.inMemoryUserAuth = inMemoryUserAuth;
+    ctx.createUserAuth = signInFactory(inMemoryUserAuth);
+  });
+
+  it('should throw an InvalidArgumentError if missing required fields', async ({
+    createUserAuth,
+  }) => {
+    await expect(createUserAuth.exec(userWithEmptyField)).rejects.toThrowError(
+      'Missing required field',
+    );
+  });
+
+  it('should throw an InvalidArgumentError if email is not valid', async ({
+    createUserAuth,
+  }) => {
+    await expect(
+      createUserAuth.exec(userWithInvalidEmail),
+    ).rejects.toThrowError('Invalid email');
+  });
+
+  it('should throw a UserNotFoundError if does not find user by email', async ({
+    createUserAuth,
+  }) => {
+    await expect(createUserAuth.exec(validUser)).rejects.toThrowError(
+      'User does not exist',
+    );
+  });
+
+  it('should throw an InvalidPasswordError if passwords do not match', async ({
+    createUserAuth,
+    inMemoryUserAuth,
+  }) => {
+    const { hashedPassword } = await hashPassword('John1#');
+    expect(hashedPassword).toBeTypeOf('string');
+    const user = {
+      email: 'jansen@gmail.com',
+      hashedPassword,
+    };
+    inMemoryUserAuth.users?.push(user);
+    expect(inMemoryUserAuth.users?.length).toBe(1);
+
+    await expect(
+      createUserAuth.exec({
+        username: 'random',
+        email: 'jansen@gmail.com',
+        password: 'wrong123',
+      }),
+    ).rejects.toThrowError('Invalid password');
+  });
+
+  it('should return a jwt', async ({ createUserAuth, inMemoryUserAuth }) => {
+    const { hashedPassword } = await hashPassword('John1#');
+    expect(hashedPassword).toBeTypeOf('string');
+    const user = {
+      userId: 'fdsfdsfsdfsd',
+      email: 'jansen@gmail.com',
+      hashedPassword,
+    };
+    inMemoryUserAuth.users?.push(user);
+    expect(inMemoryUserAuth.users?.length).toBe(1);
+
+    const jwt: string = await createUserAuth.exec({
+      username: 'random',
+      email: 'jansen@gmail.com',
+      password: 'John1#',
+    });
+    expect(jwt).toBeTypeOf('string');
+    expect(jwt.split('.').length).toBe(5);
+  });
+});
---
+++ src/services/auth/signin/signin.ts
@@ -0,0 +1,47 @@
+import validator from 'validator';
+import {
+  InvalidArgumentError,
+  UserNotFoundError,
+  InvalidPasswordError,
+} from '../../../errors/auth.errors';
+import {
+  UserAuthData,
+  UserAuthRepository,
+} from '../../../interfaces/auth.interfaces';
+import checkPasswords from '../../security/hash/validate-password';
+import { JWTPayload } from '../../../interfaces/jwt-service.interfaces';
+import { encodeJWT } from '../../security/jwt/encode';
+
+export function signInFactory(userAuthRepository: UserAuthRepository) {
+  return {
+    async exec({ email, password }: UserAuthData) {
+      if (!email || !password) {
+        throw new InvalidArgumentError('Missing required field');
+      }
+
+      if (!validator.isEmail(email)) {
+        throw new InvalidArgumentError('Invalid email');
+      }
+
+      const user = await userAuthRepository.getUserByEmail(email);
+      if (!user) {
+        throw new UserNotFoundError('User does not exist');
+      }
+
+      const passwordsMatch = await checkPasswords(
+        password,
+        user.hashedPassword,
+      );
+      if (!passwordsMatch) {
+        throw new InvalidPasswordError('Invalid password');
+      }
+
+      const payload: JWTPayload = {
+        userId: user.userId as string,
+        email: user.email,
+      };
+
+      return await encodeJWT(payload);
+    },
+  };
+}
