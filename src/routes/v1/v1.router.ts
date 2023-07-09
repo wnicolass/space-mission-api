007f672de29977403413d752cc41441e8d87e60a
wnicolass <nicolaswgl99@gmail.com>
2023-07-09 22:34:16 +0100

    feat: create planets router and attach it to the v1 router

--- src/routes/v1/v1.router.ts
+++ src/routes/v1/v1.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth.router';
+import planetsRouter from './planets.router';
 
 export default (function v1Router(): Router {
   const router = Router();
 
   router.use('/auth', authRouter);
+  router.use('/planets', planetsRouter);
 
   return router;
 })();
