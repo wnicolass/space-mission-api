3626ca3b5004126b98c5d60f549403adab6e1224
wnicolass <nicolaswgl99@gmail.com>
2023-07-17 19:41:28 +0100

    test: in memory launch repository

--- src/repositories/in-memory/in-memory.launch.repository.spec.ts
+++ src/repositories/in-memory/in-memory.launch.repository.spec.ts
@@ -23,14 +23,28 @@ function createLaunchMock(
   };
 }
 
+function createPlanetMock(): Planet {
+  return {
+    planetName: 'Kepler-442 b',
+  };
+}
+
 describe('Launch Repository Tests', () => {
   it('should return an empty array of launches', async () => {
     const sut = createSut(inMemoryLaunchRepository);
     expect(sut.getAll()).resolves.toHaveLength(0);
   });
 
-  // it('should add a new launch', async () => {
-  //   const sut = createSut(inMemoryLaunchRepository);
-  //   const planetMock =
-  // });
+  it('should add a new launch', async () => {
+    const sut = createSut(inMemoryLaunchRepository);
+    const planetMock = createPlanetMock();
+    const launchMock = createLaunchMock(
+      'Test mission',
+      'Test rocket',
+      '2023-12-04',
+      planetMock,
+    );
+    await sut.save(launchMock);
+    expect(sut.launches).toHaveLength(1);
+  });
 });
