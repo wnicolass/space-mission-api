e65f07483b3bb118cbf269d25a65b0626ef7c5bf
wnicolass <nicolaswgl99@gmail.com>
2023-07-17 20:07:22 +0100

    test: move mock function to mocks folder

--- src/repositories/in-memory/in-memory.launch.repository.spec.ts
+++ src/repositories/in-memory/in-memory.launch.repository.spec.ts
@@ -1,32 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import { inMemoryLaunchRepository } from './in-memory.launch.repository';
-import { IncomingLaunch } from '../../interfaces/launches.interfaces';
-import { Planet } from '../../interfaces/planets.interfaces';
+import { createLaunchMock } from '../../tests-data/mocks/launches';
 
 function createSut<T>(repo: () => T): T {
   const sut = repo();
 
   return sut;
-}
-
-function createLaunchMock(
-  mission: string,
-  rocket: string,
-  launchDate: string,
-  planet: Planet,
-): IncomingLaunch {
-  return {
-    mission,
-    rocket,
-    launchDate,
-    planet,
-  };
-}
-
-function createPlanetMock(): Planet {
-  return {
-    planetName: 'Kepler-442 b',
-  };
 }
 
 describe('Launch Repository Tests', () => {
@@ -37,12 +16,10 @@ describe('Launch Repository Tests', () => {
 
   it('should add a new launch', async () => {
     const sut = createSut(inMemoryLaunchRepository);
-    const planetMock = createPlanetMock();
     const launchMock = createLaunchMock(
       'Test mission',
       'Test rocket',
       '2023-12-04',
-      planetMock,
     );
     await sut.save(launchMock);
     expect(sut.launches).toHaveLength(1);
