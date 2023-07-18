79cb70c7f59b2a5e8c5b8c66be838d3fce44f646
wnicolass <nicolaswgl99@gmail.com>
2023-07-18 19:14:34 +0100

    refactor: rename 'auth repository' to 'user repository'

---
+++ src/repositories/in-memory/in-memory.user.repository.ts
@@ -0,0 +1,38 @@
+import { randomUUID } from 'node:crypto';
+import type {
+  InDatabaseUser,
+  UserAuthRepository,
+} from '../../interfaces/user.interfaces';
+
+export default function inMemoryUserAuthRepository(): UserAuthRepository {
+  return {
+    users: [],
+    signup({ email, username, password }) {
+      return new Promise((res) => {
+        const newUser: InDatabaseUser = {
+          email,
+          username,
+          hashedPassword: password,
+          userId: randomUUID(),
+        };
+        this.users?.push(newUser);
+        res();
+      });
+    },
+    getUserByEmail(email) {
+      return new Promise((res) => {
+        const userFound = this.users?.find((user) => user.email === email);
+        if (!userFound) {
+          return res();
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        return res(userFound);
+      });
+    },
+    getUserById(userId) {
+      return new Promise((res) => {
+        res(this.users?.find((user) => user.userId === userId));
+      });
+    },
+  };
+}
