d4e7dc0960c6a0bb21e57999cd044506d4d75480
wnicolass <nicolaswgl99@gmail.com>
2023-07-25 15:00:52 +0100

    fix: missing resolve promise

--- src/repositories/in-memory/in-memory.userlaunch.repository.ts
+++ src/repositories/in-memory/in-memory.userlaunch.repository.ts
@@ -11,7 +11,7 @@ export function inMemoryUserLaunchRepository(): UserLaunchRepository {
         if (expedition) {
           return res(expedition);
         }
-        return null;
+        return res(null);
       });
     },
     getExpeditionByLaunchAndUserId(launchId, userId) {
@@ -23,7 +23,7 @@ export function inMemoryUserLaunchRepository(): UserLaunchRepository {
         if (expedition) {
           return res(expedition);
         }
-        return null;
+        return res(null);
       });
     },
   };
