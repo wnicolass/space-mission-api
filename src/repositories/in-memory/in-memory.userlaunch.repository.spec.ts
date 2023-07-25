c1cd7d38b3e189f8504bdff741f15464b4006d40
wnicolass <nicolaswgl99@gmail.com>
2023-07-25 14:23:48 +0100

    test: create test for new function 'UserLaunch' repository

--- src/repositories/in-memory/in-memory.userlaunch.repository.spec.ts
+++ src/repositories/in-memory/in-memory.userlaunch.repository.spec.ts
@@ -3,7 +3,7 @@ import { inMemoryUserLaunchRepository } from './in-memory.userlaunch.repository'
 
 describe('In Memory UserLaunch Repository', () => {
   const sut = inMemoryUserLaunchRepository();
-  it('should find a launch by id', async () => {
+  it('should find an expedition by launch id', async () => {
     const expeditionMock = {
       userId: 'sffsfaf',
       launchId: '123',
@@ -13,5 +13,17 @@ describe('In Memory UserLaunch Repository', () => {
     expect(sut.getExpeditionByLaunchId('123')).resolves.toStrictEqual(
       expeditionMock,
     );
+  });
+
+  it('should find an expedition by launch and user id', async () => {
+    const expeditionMock = {
+      userId: '321',
+      launchId: '123',
+      launchDate: new Date('2025-12-12'),
+    };
+    sut.expeditions?.push(expeditionMock);
+    expect(
+      sut.getExpeditionByLaunchAndUserId('123', '321'),
+    ).resolves.toStrictEqual(expeditionMock);
   });
 });
