dfddf2cedd636dc133eb1309ea215831363eb36a
wnicolass <nicolaswgl99@gmail.com>
2023-07-27 16:24:52 +0100

    feat: end tests of the 'joinLaunchService'

--- src/controllers/launches/join/join-launch.controller.spec.ts
+++ src/controllers/launches/join/join-launch.controller.spec.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect, beforeAll } from 'vitest';
+import { Launch, UserAuthData } from '@prisma/client';
 import request from 'supertest';
+import userAuthRepositoryFactory from '../../../repositories/prisma/prisma-user-auth.repository';
 import app from '../../../app';
 import { createDbUserMock } from '../../../tests/mocks/user';
-import userAuthRepositoryFactory from '../../../repositories/prisma/prisma-user-auth.repository';
 import { signInFactory } from '../../../services/auth/signin/signin';
-import { UserAuthData } from '@prisma/client';
+import { createDbLaunchMock } from '../../../tests/mocks/launches';
 
 describe('Join Launch Controller', () => {
   const LAUNCHES_URL = '/v1/launches';
@@ -32,6 +33,59 @@ describe('Join Launch Controller', () => {
 
     expect(response.body).toStrictEqual({
       error: 'Launch not found',
+    });
+  });
+
+  it('should respond with 400 bad request if user already joined on launch', async () => {
+    const userMock = await createDbUserMock(
+      'testarino@gmail.com',
+      'Testarino1#',
+      'testarino',
+    );
+    const launchMock = (await createDbLaunchMock(
+      'Testarino mission',
+      'TestarinoRocket',
+      '2028-12-12',
+      userMock.userId,
+    )) as Launch;
+    const response = await request(app)
+      .post(`${LAUNCHES_URL}/${launchMock.launchId}`)
+      .set('authorization', `Bearer ${jwt}`)
+      .send({ userId: userMock.userId })
+      .expect('Content-Type', /json/i)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'User already joined',
+    });
+  });
+
+  it('should respond with 200 ok and a success message', async () => {
+    const userMock = await createDbUserMock(
+      'testarino2@gmail.com',
+      'Testarino2#',
+      'testarino2',
+    );
+    const userMock2 = await createDbUserMock(
+      'thetester@gmail.com',
+      'Thetester1#',
+      'the thester',
+    );
+    const launchMock = (await createDbLaunchMock(
+      'Testarino mission',
+      'TestarinoRocket',
+      '2028-12-12',
+      userMock.userId,
+    )) as Launch;
+    const response = await request(app)
+      .post(`${LAUNCHES_URL}/${launchMock.launchId}`)
+      .set('authorization', `Bearer ${jwt}`)
+      .send({ userId: userMock2.userId })
+      .expect('Content-Type', /json/i)
+      .expect(200);
+
+    expect(response.body).toStrictEqual({
+      message: 'Successfully joined launch',
     });
   });
 });
