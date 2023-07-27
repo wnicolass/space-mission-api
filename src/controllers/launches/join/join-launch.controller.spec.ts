4a4cfd06f1e4571d7898655dce250e3bc807cdcd
wnicolass <nicolaswgl99@gmail.com>
2023-07-27 16:01:15 +0100

    test: add jwt to pass authentication middleware

--- src/controllers/launches/join/join-launch.controller.spec.ts
+++ src/controllers/launches/join/join-launch.controller.spec.ts
@@ -1,12 +1,31 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import request from 'supertest';
 import app from '../../../app';
+import { createDbUserMock } from '../../../tests/mocks/user';
+import userAuthRepositoryFactory from '../../../repositories/prisma/prisma-user-auth.repository';
+import { signInFactory } from '../../../services/auth/signin/signin';
+import { UserAuthData } from '@prisma/client';
 
 describe('Join Launch Controller', () => {
   const LAUNCHES_URL = '/v1/launches';
+  let jwt = '';
+  let dbUserMock: UserAuthData;
+
+  beforeAll(async () => {
+    dbUserMock = await createDbUserMock('test2@test2.com', 'Test2#', 'tester2');
+    const userRepository = userAuthRepositoryFactory();
+    const signInService = signInFactory(userRepository);
+    jwt = await signInService.exec({
+      username: '',
+      email: dbUserMock.email,
+      password: 'Test2#',
+    });
+  });
+
   it('should respond with 404 not found if does not find launch to join', async () => {
     const response = await request(app)
       .post(`${LAUNCHES_URL}/123`)
+      .set('authorization', `Bearer ${jwt}`)
       .send({ userId: '321' })
       .expect('Content-Type', /json/i)
       .expect(404);
