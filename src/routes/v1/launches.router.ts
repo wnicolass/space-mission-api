4bd675dc2a6db99f9f63edaf9e97798c391e1749
wnicolass <nicolaswgl99@gmail.com>
2023-07-31 23:31:21 +0100

    feat: attach new 'getAllLaunchesController' on the v1 router

--- src/routes/v1/launches.router.ts
+++ src/routes/v1/launches.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import saveLaunchController from '../../controllers/launches/save/save-launch.controller';
 import joinLaunchController from '../../controllers/launches/join/join-launch.controller';
+import getAllLaunchesController from '../../controllers/launches/get-all/get-all-launches.controller';
 import checkAuthStatus from '../../middlewares/check-auth-status';
 
 export default (function launchesRouter(): Router {
   const router = Router();
 
+  router.get('/', getAllLaunchesController.exec);
   router.use(checkAuthStatus);
   router.post('/', saveLaunchController.exec);
   router.post('/:launchId', joinLaunchController.exec);
