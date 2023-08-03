622cf8d448e8ef735a8ed68fe2d021e1ce4b2692
wnicolass <nicolaswgl99@gmail.com>
2023-08-03 18:07:18 +0100

    feat: attach 'abortLaunchController' to the 'v1Router'

--- src/routes/v1/launches.router.ts
+++ src/routes/v1/launches.router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import saveLaunchController from '../../controllers/launches/save/save-launch.controller';
 import joinLaunchController from '../../controllers/launches/join/join-launch.controller';
 import getAllLaunchesController from '../../controllers/launches/get-all/get-all-launches.controller';
+import abortLaunchController from '../../controllers/launches/abort/abort-launch.controller';
 import checkAuthStatus from '../../middlewares/check-auth-status';
 
 export default (function launchesRouter(): Router {
@@ -11,6 +12,7 @@ export default (function launchesRouter(): Router {
   router.use(checkAuthStatus);
   router.post('/', saveLaunchController.exec);
   router.post('/:launchId', joinLaunchController.exec);
+  router.delete('/', abortLaunchController.exec);
 
   return router;
 })();
