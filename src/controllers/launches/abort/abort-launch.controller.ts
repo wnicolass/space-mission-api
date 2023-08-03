bc709f0b985259f4567a95fc1b4c77404ec3c61f
wnicolass <nicolaswgl99@gmail.com>
2023-08-03 18:06:39 +0100

    feat: create 'abortLaunchController'

---
+++ src/controllers/launches/abort/abort-launch.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '../../../interfaces/controllers.interfaces';
+import { abortLaunchFactory } from '../../../services/launches/abort/abort-launch';
+import launchRepositoryFactory from '../../../repositories/prisma/prisma-launch.repository';
+
+export default (function abortLaunchController(): Controller {
+  return {
+    async exec({ body }, res, next) {
+      try {
+        const launchesRepository = launchRepositoryFactory();
+        const abortLaunchService = abortLaunchFactory(launchesRepository);
+        await abortLaunchService.exec(body.launchId);
+        return res.status(200).json({
+          message: 'Successfully aborted launch',
+        });
+      } catch (err: unknown) {
+        return next(err);
+      }
+    },
+  };
+})();
