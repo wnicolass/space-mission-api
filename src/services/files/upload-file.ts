d3fb017d4482358cc4a43dc0285f9f92a108293b
wnicolass <nicolaswgl99@gmail.com>
2023-08-06 23:41:12 +0100

    fix: create readable stream from file buffer and pipe to cloudinary upload stream

--- src/services/files/upload-file.ts
+++ src/services/files/upload-file.ts
@@ -1,16 +1,28 @@
+import { Readable } from 'node:stream';
 import { cloudinary } from '../../config/cloudinary-config';
 import { UserAuthRepository } from '../../interfaces/user.interfaces';
 
 export function uploadFileFactory(userRepository: UserAuthRepository) {
   return {
     async exec(userId: string, fileBuffer: Buffer) {
-      const { secure_url: secureUrl } = await cloudinary.uploader.upload(
-        fileBuffer.toString(),
-      );
-      await userRepository.updateProfile(userId, {
-        profileImageUrl: secureUrl,
+      return new Promise((res, rej) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { resource_type: 'image' },
+          async (err, result) => {
+            if (err) return rej(err);
+
+            if (result) {
+              await userRepository.updateProfile(userId, {
+                profileImageUrl: result.secure_url,
+              });
+
+              return res(result);
+            }
+          },
+        );
+
+        Readable.from(fileBuffer).pipe(uploadStream);
       });
-      return secureUrl;
     },
   };
 }
