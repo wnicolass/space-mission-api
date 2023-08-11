17fdb6a4e61b7d1efe5a50e62209a0b6bced1ab1
wnicolass <nicolaswgl99@gmail.com>
2023-08-11 16:07:31 +0100

    feat: add validation on the 'launch.createdBy' field

--- src/services/launches/abort/abort-launch.ts
+++ src/services/launches/abort/abort-launch.ts
@@ -1,17 +1,24 @@
+import { ForbiddenError } from '../../../errors/auth.errors';
 import { LaunchNotFoundError } from '../../../errors/launch.errors';
 import { LaunchRepository } from '../../../interfaces/launches.interfaces';
 
 export function abortLaunchFactory(launchesRepository: LaunchRepository) {
   return {
-    async exec(launchId: string) {
+    async exec(launchId: string, userId: string) {
       const launch = await launchesRepository.getLaunchById(launchId);
 
       if (!launch) {
         throw new LaunchNotFoundError('Launch not found');
       }
 
+      if (userId !== launch.createdBy) {
+        throw new ForbiddenError(
+          'Only the user who have created the launch can abort it',
+        );
+      }
+
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return await launchesRepository.abort(launch.launchId!);
+      return await launchesRepository.abort(launch.launchId!, userId);
     },
   };
 }
