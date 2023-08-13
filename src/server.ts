2ae1c41d7f8058bea23beb3365fb1e338b791402
wnicolass <nicolaswgl99@gmail.com>
2023-08-13 17:16:05 +0100

    chore: setup apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
 import { createServer } from 'node:https';
+import bodyParser from 'body-parser';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { expressMiddleware } from '@apollo/server/express4';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import { tlsConfig } from './config/https/server-config';
 import app from './app';
 
-(function startServer(): void {
+(async function startServer() {
   const { PORT } = process.env;
+  const graphqlTypes = loadFilesSync('**/*', {
+    extensions: ['graphql'],
+  });
+  const resolvers = loadFilesSync('**/*.resolvers.ts');
+  const schema = makeExecutableSchema({
+    typeDefs: graphqlTypes,
+    resolvers,
+  });
+  const httpServer = createServer(tlsConfig, app);
+  const apolloServer = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await apolloServer.start();
 
-  const server = createServer(tlsConfig, app);
-  server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  app.use('/graphql', bodyParser.json(), expressMiddleware(apolloServer));
+  httpServer.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}`),
+  );
 })();
