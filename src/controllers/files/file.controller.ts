7b64a6bff2383fc52e1a20626f038f98214ffb64
wnicolass <nicolaswgl99@gmail.com>
2023-08-19 15:00:04 +0100

    fix: send userId from the request object

--- src/controllers/files/file.controller.ts
+++ src/controllers/files/file.controller.ts
@@ -4,12 +4,15 @@ import { uploadFileFactory } from '../../services/files/upload-file';
 
 export default (function uploadFileController(): Controller {
   return {
-    async exec({ body, file }, res, next) {
+    async exec(req, res, next) {
       try {
         const userRepository = userAuthRepositoryFactory();
         const uploadFileService = uploadFileFactory(userRepository);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const result = await uploadFileService.exec(body.userId, file!.buffer);
+        const result = await uploadFileService.exec(
+          req.user.userId,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          req.file!.buffer,
+        );
         return res.status(200).json({
           message: 'Successfully updated profile image',
           publicId: result.public_id,
