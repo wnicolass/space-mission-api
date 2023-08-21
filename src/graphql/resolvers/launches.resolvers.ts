c779c3fb105da3c6ed95fbd485f5377a2178b0a9
wnicolass <nicolaswgl99@gmail.com>
2023-08-21 19:23:43 +0100

    feat: create 'saveLaunch' mutation on 'launchesResolvers'

--- src/graphql/resolvers/launches.resolvers.ts
+++ src/graphql/resolvers/launches.resolvers.ts
@@ -1,12 +1,55 @@
 import launchRepositoryFactory from '../../repositories/prisma/prisma-launch.repository';
+import planetRepositoryFactory from '../../repositories/prisma/prisma-planet.repository';
+import userAuthRepositoryFactory from '../../repositories/prisma/prisma-user-auth.repository';
+import { UserContext } from '../context';
+import { saveLaunchFactory } from '../../services/launches/save/save';
+import { ensureUserInContext } from '../helpers/validate-context-user';
+import { getAllLaunchesFactory } from '../../services/launches/get-all/get-all-launches';
+
+type IncomingLaunchGQL = {
+  mission: string;
+  rocket: string;
+  launchDate: string;
+  planet: string;
+};
 
 export default (function launchesResolvers() {
   const launchRepository = launchRepositoryFactory();
+  const planetRepository = planetRepositoryFactory();
+  const userRepository = userAuthRepositoryFactory();
 
   return {
     Query: {
       launches: async () => {
-        return await launchRepository.getAll();
+        const getAllLaunchesService = getAllLaunchesFactory(launchRepository);
+        return await getAllLaunchesService.exec();
+      },
+    },
+    Mutation: {
+      saveLaunch: async (
+        _: unknown,
+        args: IncomingLaunchGQL,
+        context: UserContext,
+      ) => {
+        ensureUserInContext(context);
+        const newLaunch = {
+          mission: args.mission,
+          rocket: args.rocket,
+          launchDate: args.launchDate,
+          planet: {
+            planetName: args.planet,
+          },
+          userId: context.user!.userId,
+        };
+        const saveLaunchService = saveLaunchFactory(
+          launchRepository,
+          userRepository,
+          planetRepository,
+        );
+        await saveLaunchService.exec(newLaunch);
+        return {
+          message: 'Launch successfully created',
+        };
       },
     },
   };
