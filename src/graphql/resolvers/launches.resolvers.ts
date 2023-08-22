a2aaa1309bc8f3cc999225ec8bf40328016d7beb
wnicolass <nicolaswgl99@gmail.com>
2023-08-22 15:33:13 +0100

    feat: create resolver for 'joinLaunch' mutation

--- src/graphql/resolvers/launches.resolvers.ts
+++ src/graphql/resolvers/launches.resolvers.ts
@@ -3,8 +3,10 @@ import planetRepositoryFactory from '../../repositories/prisma/prisma-planet.rep
 import userAuthRepositoryFactory from '../../repositories/prisma/prisma-user-auth.repository';
 import { UserContext } from '../context';
 import { saveLaunchFactory } from '../../services/launches/save/save';
+import { joinLaunchFactory } from '../../services/launches/join/join-launch';
 import { ensureUserInContext } from '../helpers/validate-context-user';
 import { getAllLaunchesFactory } from '../../services/launches/get-all/get-all-launches';
+import userLaunchRepositoryFactory from '../../repositories/prisma/prisma-userlaunch.repository';
 
 type IncomingLaunchGQL = {
   mission: string;
@@ -14,15 +16,18 @@ type IncomingLaunchGQL = {
 };
 
 export default (function launchesResolvers() {
+  const userRepository = userAuthRepositoryFactory();
   const launchRepository = launchRepositoryFactory();
   const planetRepository = planetRepositoryFactory();
-  const userRepository = userAuthRepositoryFactory();
+  const userLaunchRepository = userLaunchRepositoryFactory();
 
   return {
     Query: {
       launches: async () => {
         const getAllLaunchesService = getAllLaunchesFactory(launchRepository);
-        return await getAllLaunchesService.exec();
+        const launches = await getAllLaunchesService.exec();
+        console.log(launches);
+        return launches;
       },
     },
     Mutation: {
@@ -49,6 +54,22 @@ export default (function launchesResolvers() {
         await saveLaunchService.exec(newLaunch);
         return {
           message: 'Launch successfully created',
+        };
+      },
+      joinLaunch: async (
+        _: unknown,
+        args: Record<'launchId', string>,
+        context: UserContext,
+      ) => {
+        ensureUserInContext(context);
+        const joinLaunchService = joinLaunchFactory(userLaunchRepository);
+        const joinLaunchData = {
+          userId: context.user!.userId,
+          launchId: args.launchId,
+        };
+        await joinLaunchService.exec(joinLaunchData);
+        return {
+          message: 'Successfully joined launch',
         };
       },
     },
