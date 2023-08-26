25a04d19de856fbc0b87a6ed2288f1ab56b7e236
wnicolass <nicolaswgl99@gmail.com>
2023-08-26 23:36:12 +0100

    feat: end 'abortLaunch' resolver

--- src/graphql/resolvers/launches.resolvers.ts
+++ src/graphql/resolvers/launches.resolvers.ts
@@ -7,6 +7,7 @@ import { saveLaunchFactory } from '../../services/launches/save/save';
 import { joinLaunchFactory } from '../../services/launches/join/join-launch';
 import { ensureUserInContext } from '../helpers/validate-context-user';
 import { getAllLaunchesFactory } from '../../services/launches/get-all/get-all-launches';
+import { abortLaunchFactory } from '../../services/launches/abort/abort-launch';
 
 type IncomingLaunchGQL = {
   mission: string;
@@ -72,12 +73,17 @@ export default (function launchesResolvers() {
           message: 'Successfully joined launch',
         };
       },
-      abortLaunch: (
+      abortLaunch: async (
         _: unknown,
         args: Record<'launchId', string>,
         context: UserContext,
       ) => {
         ensureUserInContext(context);
+        const abortLaunchService = abortLaunchFactory(launchRepository);
+        await abortLaunchService.exec(args.launchId, context.user!.userId);
+        return {
+          message: 'Successfully aborted launch',
+        };
       },
     },
   };
