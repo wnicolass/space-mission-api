693b282abc9ddfb3f1ce42f69a56fe3f48124ba3
wnicolass <nicolaswgl99@gmail.com>
2023-08-28 14:33:48 +0100

    feat: update user interface

--- src/interfaces/user.interfaces.ts
+++ src/interfaces/user.interfaces.ts
@@ -21,7 +21,7 @@ export type UserAuthRepository = {
   users?: InDatabaseUser[];
   signup(data: UserAuthData): Promise<void>;
   getUserByEmail(email: string): Promise<InDatabaseUser | void>;
-  getUserById(userId: string): Promise<InDatabaseUser | void>;
+  getUserById(userId: string): Promise<InDatabaseUser | void> | any;
   updateProfile(userId: string, newData: UserProfile): Promise<UserProfile>;
 };
 
