acec2013276b2fd8e83f182a318e572fd25b2d63
wnicolass <nicolaswgl99@gmail.com>
2023-08-31 14:42:03 +0100

    feat: attach swagger to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { createServer } from 'node:https';
 import { tlsConfig } from './config/https/server-config';
+import { setupSwagger } from './config/swagger/swagger-setup';
 import { setupApolloServer } from './graphql/setup-apollo';
 import app from './app';
 
@@ -7,6 +8,7 @@ import app from './app';
   const { PORT } = process.env;
   const httpServer = createServer(tlsConfig, app);
   await setupApolloServer(httpServer, app);
+  await setupSwagger(app);
   httpServer.listen(PORT, () =>
     console.log(`Server listening on port ${PORT}`),
   );
